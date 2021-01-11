a264560da4b2ec12c6ad4836615a9bafb68ef15c
Henry Ning <henryhuning@gmail.com>
2021-01-11 08:33:42 +0000

    Quiz 6: Code Indentation Quiz

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ def my_function():
     print("blue")
 
 print("world")
+
+# What you knowThese are questions you got right on the first try.
+# Which version of code will produce an Indentation Error when it is run?NOTE: The • symbol represents a single space.
+# Which version of code will output "This will run"?
+# In which version of code, will you see "This will run" printed?
